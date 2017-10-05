172163e012f0def59cac0852fc02f6457a25c497
Esteban Artavia <Esteban Artavia@MCKINSEY.COM>
2017-10-04 22:35:19 -0600

    Models, guardian y student

--- LiftingEnkiMatriculas/models.py
+++ LiftingEnkiMatriculas/models.py
@@ -17,9 +17,9 @@ class student_obj(models.Model):
     student_gender = models.CharField(max_length=200)
     student_phone = models.CharField(max_length=8)
     student_address = models.TextField(max_length=800)
-    guardian1 = models.ForeignKey(guardian1_obj, on_delet=models.CASCADE)
+    studen_guardian = models.ManytoManyField (guardian_obj)
 
-class guardian1_obj(models.Model):
+class guardian_obj(models.Model):
     guardian1_id = models.CharField(max_length=200)
     guardian1_name = models.CharField(max_length=200)
     guardian1_middlename = models.CharField(max_length=200)
@@ -28,14 +28,3 @@ class guardian1_obj(models.Model):
     guardian1_2ndlastname = models.CharField(max_length=200)
     guardian1_phone = models.CharField(max_length=8)
     guardian1_relation = models.CharField(max_length=200)
-
-#class guardian2_obj(models.Model):
-#    guardian2_name = models.CharField(max_length=30)
-#    guardian2_middlename = models.CharField(max_length=30)
-#    guardian2_lastname = models.CharField(max_length=30)
-#    guardian2_2ndlastname = models.CharField(max_length=30)
-#    guardian2_2ndlastname = models.CharField(max_length=30)
-#    guardian2_phone = models.CharField(max_length=8)
-
-
-
