7ba43fe7fa0a34d02196b325ceec026c4bc549ef
Esteban Artavia <Esteban Artavia@MCKINSEY.COM>
2017-10-05 12:33:23 -0600

    correction of model array

--- LiftingEnkiMatriculas/models.py
+++ LiftingEnkiMatriculas/models.py
@@ -4,21 +4,6 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
-
-class student_obj(models.Model):
-    student_id = models.CharField(max_length=200)
-    student_name = models.CharField(max_length=200)
-    student_middlename = models.CharField(max_length=200)
-    student_lastname = models.CharField(max_length=200)
-    student_2ndlastname = models.CharField(max_length=200)
-    student_2ndlastname = models.CharField(max_length=200)
-    student_birthday = models.DateTimeField('Cumpleanos')
-    student_grade = models.CharField(max_length=200)
-    student_gender = models.CharField(max_length=200)
-    student_phone = models.CharField(max_length=8) #look later for correct storing method
-    student_address = models.TextField(max_length=800)
-    student_guardian = models.ManytoManyField (guardian_obj)
-
 class guardian_obj(models.Model):
     guardian_id = models.CharField(max_length=200)
     guardian_name = models.CharField(max_length=200)
@@ -29,7 +14,29 @@ class guardian_obj(models.Model):
     guardian_phone = models.CharField(max_length=8) #look later for correct storing method
     guardian_relation = models.CharField(max_length=200)
 
-class course_obj(models.Model) 
+
+class profesor_obj(models.Model):
+    profesor_id = models.CharField(max_length=200)
+    profesor_name = models.CharField(max_length=200)
+    profesor_lastname = models.CharField(max_length=200)
+    profesor_phone = models.CharField(max_length=8) #look later for correct storing method
+
+
+class student_obj(models.Model):
+    student_id = models.CharField(max_length=200)
+    student_name = models.CharField(max_length=200)
+    student_middlename = models.CharField(max_length=200)
+    student_lastname = models.CharField(max_length=200)
+    student_2ndlastname = models.CharField(max_length=200)
+    student_2ndlastname = models.CharField(max_length=200)
+    student_birthday = models.DateTimeField()
+    student_grade = models.CharField(max_length=200)
+    student_gender = models.CharField(max_length=200)
+    student_phone = models.CharField(max_length=8) #look later for correct storing method
+    student_address = models.TextField(max_length=800)
+    student_guardian = models.ManytoManyField (guardian_obj) 
+
+class course_obj(models.Model):
     course_name = models.CharField(max_length=200)
     course_room = models.CharField(max_length=200)
     course_profesor = models.ForeignKey (profesor_obj, on_delete=models.CASCADE)
@@ -38,12 +45,3 @@ class course_obj(models.Model)
     course_seats = models.DecimalField(max_digits=5, decimal_places=5)
     course_gender = models.CharField(max_length=200)
     cours_grade = models.CharField(max_length=200)
-
-
-class profesor_obj(models.Model)
-    profesor_id = models.CharField(max_length=200)
-    profesor_name = models.CharField(max_length=200)
-    profesor_lastname = models.CharField(max_length=200)
-    profesor_phone = models.CharField(max_length=8) #look later for correct storing method
-    
-
