0b3693a6f0f1227003c0172ffcd9d9d67b3489fb
Esteban Artavia <Esteban Artavia@MCKINSEY.COM>
2017-10-05 16:25:08 -0600

    Added admin access for fields

--- LiftingEnkiMatriculas/admin.py
+++ LiftingEnkiMatriculas/admin.py
@@ -4,3 +4,12 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
+from .models import student_obj
+from .models import profesor_obj
+from .models import course_obj
+from .models import guardian_obj
+
+admin.site.register(student_obj)
+admin.site.register(profesor_obj)
+admin.site.register(guardian_obj)
+admin.site.register(course_obj)
