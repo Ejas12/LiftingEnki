ca60bdef5c5c0a944c3e3cc7bfdd17a82780ed1b
Esteban Artavia <Esteban Artavia@MCKINSEY.COM>
2017-10-07 12:57:43 -0600

    model correction

--- LiftingEnkiMatriculas/models.py
+++ LiftingEnkiMatriculas/models.py
@@ -11,7 +11,7 @@ class guardian_obj(models.Model):
     guardian_lastname = models.CharField(max_length=200)
     guardian_2ndlastname = models.CharField(max_length=200)
     guardian_2ndlastname = models.CharField(max_length=200)
-    guardian_phone = models.DecimalField(max_digits=8, decimal_places=7) #look later for correct storing method
+    guardian_phone = models.DecimalField(max_digits=8, decimal_places=0) #look later for correct storing method
     guardian_relation = models.CharField(max_length=200)
     def __str__(self):
         return '%s %s' % (self.guardian_name, self.guardian_lastname)
@@ -21,7 +21,7 @@ class profesor_obj(models.Model):
     profesor_id = models.CharField(max_length=200)
     profesor_name = models.CharField(max_length=200)
     profesor_lastname = models.CharField(max_length=200)
-    profesor_phone = models.CharField(max_length=8) #look later for correct storing method
+    profesor_phone = models.DecimalField(max_digits=8, decimal_places=0)#look later for correct storing method
     def __str__(self):
         return '%s %s' % (self.profesor_name, self.profesor_lastname)
 
@@ -36,7 +36,7 @@ class student_obj(models.Model):
     student_birthday = models.DateField()
     student_grade = models.CharField(max_length=200)
     student_gender = models.CharField(max_length=200)
-    student_phone = models.CharField(max_length=8) #look later for correct storing method
+    student_phone = models.DecimalField(max_digits=8, decimal_places=0)#look later for correct storing method
     student_address = models.TextField(max_length=800)
     student_guardian = models.ManyToManyField (guardian_obj)
     def __str__(self):
